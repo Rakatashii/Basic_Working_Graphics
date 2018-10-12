685b8757e6371708176a225056f62a7a591d6c45
Christian Meyer <christianmeyer@christians-macbook-pro.local>
2018-10-12 18:49:56 -0400

    Initial Commit

---
+++ Build/Intermediates.noindex/Horstmann.build/Debug/Horstmann.build/Objects-normal/x86_64/ccc_x11.d
@@ -0,0 +1,8 @@
+dependencies: /Users/christianmeyer/Horstmann\ 2/ccc_x11.cpp \
+  /Users/christianmeyer/Horstmann\ 2/ccc_x11.h \
+  /usr/X11/include/X11/Xlib.h /usr/X11/include/X11/X.h \
+  /usr/X11/include/X11/Xfuncproto.h /usr/X11/include/X11/Xosdefs.h \
+  /usr/X11/include/X11/Xutil.h /usr/X11/include/X11/keysym.h \
+  /usr/X11/include/X11/keysymdef.h /usr/X11/include/X11/Xatom.h \
+  /usr/X11/include/X11/Xos.h /usr/X11/include/X11/Xarch.h \
+  /Users/christianmeyer/Horstmann\ 2/ccc_shap.h
---
+++ Build/Intermediates.noindex/Horstmann_Master.build/Debug/Horstmann_Master.build/Objects-normal/x86_64/ccc_shap.d
@@ -0,0 +1,2 @@
+dependencies: /Users/christianmeyer/Horstmann_Master/ccc_shap.cpp \
+  /Users/christianmeyer/Horstmann_Master/ccc_shap.h
