2fc283b3365a03ca435e3290481b839f173e0438
Christian Meyer <cmeyerwps@gmail.com>
2018-11-09 14:18:52 -0500

    P8_10

--- Build/Intermediates.noindex/Horstmann_Master.build/Debug/Horstmann_Master.build/Objects-normal/x86_64/ccc_shap.d
+++ Build/Intermediates.noindex/Horstmann_Master.build/Debug/Horstmann_Master.build/Objects-normal/x86_64/ccc_shap.d
@@ -1,3 +1,3 @@
 dependencies: \
-  /Users/christianmeyer/Horstmann_Master_Template/ccc_shap.cpp \
-  /Users/christianmeyer/Horstmann_Master_Template/ccc_shap.h
+  /Users/christianmeyer/Cpp/ch8/Horstmann_Master_Template/ccc_shap.cpp \
+  /Users/christianmeyer/Cpp/ch8/Horstmann_Master_Template/ccc_shap.h
